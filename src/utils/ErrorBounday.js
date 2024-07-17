29ebae6891473c1f275182592889ac5089fad883
Dhruv <dhruvanandsingh2004@gmail.com>
2024-07-17 16:00:25 +0530

    Fix issue #1 and #2

--- src/utils/ErrorBounday.js
+++ src/utils/ErrorBounday.js
@@ -8,15 +8,18 @@ class ErrorBoundary extends React.Component {
   }
 
   static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
   componentDidCatch(error, errorInfo) {
+    // Log the error to an error reporting service
     console.error("Error caught by ErrorBoundary: ", error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
+      // Render fallback UI
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text style={{ fontSize: 18, color: 'red' }}>Something went wrong.</Text>
